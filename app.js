039e1394e7a63fd5d086479073931657937232e5
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-03-31 09:18:43 +0400

    deployed cors all origins

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const sequelize = require("./src/connections/db_connection")
 const router = require('./src/routers/index');
 const app = new Koa();
 const koaBody = require("koa-body");
-
+const cors = require('koa2-cors');
 app.use(koaBody());
 app.use(router.routes());
-
+app.use(cors({origin:"*"}))
 app.use((ctx) => {    ctx.body = "hello"}) //this will be removed
 const PORT = process.env.PORT || 3000;
 
