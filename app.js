5a80aa9b2479d862aff32715fe461b5d14e8872f
KhachatryanArt <arthur.khacahtryan@inomma.com>
2022-03-31 14:52:40 +0400

    env

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const cors = require('koa2-cors');
 app.use(koaBody());
 app.use(router.routes());
 app.use(cors({origin:"*"}))
-app.use((ctx) => {    ctx.body = "hello"}) //this will be removed
 const PORT = process.env.PORT || 3000;
 
 sequelize.authenticate().then(() => {
