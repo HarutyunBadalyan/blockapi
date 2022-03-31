bd5c82f616dbae2ec90433df3f8564c3414a7549
KhachatryanArt <arthur.khacahtryan@inomma.com>
2022-03-31 16:48:59 +0400

    Email error

--- src/middlewares/loginValidationMiddleware.js
+++ src/middlewares/loginValidationMiddleware.js
@@ -1,9 +1,15 @@
-const  compose = require('koa-compose');
-const { validationResults,body }  = require("koa-req-validation");
+const compose = require('koa-compose');
+const {body, validationResults} = require("koa-req-validation");
 const validationArray = [
-    body("email").isEmail().withMessage("Password must have 8 character").build(),
-    body("password").isLength({min:8}).withMessage("Password must have 8 character").build(),
-   
+    body("email").isEmail().withMessage("Invalid Email").build(),
+    body("password").isLength({min: 8}).withMessage("Password must have 8 character").build(),
+    (ctx, next) => {
+        const result = validationResults(ctx);
+        if (!result.results.length) {
+            return next();
+        }
+        ctx.body = result.results;
+    }
 ];
 
 let loginValidationMiddleware = compose(validationArray);
--- src/middlewares/registerValidationMiddleware.js
+++ src/middlewares/registerValidationMiddleware.js
@@ -3,7 +3,7 @@ const { validationResults,body }  = require("koa-req-validation");
 const validationArray = [
     body("firstName").isLength({min:1}).withMessage("What's your name").build(),
     body("lastName").isLength({min:1}).withMessage("What's your lastname").build(),
-    body("email").isEmail().withMessage("Password must have 8 character").build(),
+    body("email").isEmail().withMessage("Invalid Email").build(),
     body("password").isLength({min:8}).withMessage("Password must have 8 character").build(),
     body('confirmPassword').custom((input,ctx) => {
 
