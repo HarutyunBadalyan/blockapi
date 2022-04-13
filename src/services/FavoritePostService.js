061cc7851d87a71195613244faa4b501d8b33ebe
HarutyunBadalyan <harutyun.badalyan@inomma.com>
2022-04-13 16:29:28 +0400

    favpost error

--- src/services/FavoritePostService.js
+++ src/services/FavoritePostService.js
@@ -28,22 +28,30 @@ class FavoritePostService {
 
     static async getFavPost(user_id, limit, offset) {
 
-        const [value, metadata] = await FavPost.sequelize.query(`SELECT "favorite_posts"."user_id",
-                    "favorite_posts"."post_id",
-                    "Posts"."title",
-                    "Users"."firstName" ,
-                    "Users"."lastName" ,
-                    "Posts"."subtitle",
-                    "Posts"."description",
-                    "Posts"."image_url",
-                    "comments"."text"      AS "Comment_text",
-                    "comments"."parent_id" AS "Comment_parent_id",
-                    "Posts"."createdAt"
-                        FROM "favorite_posts"
-                                 LEFT JOIN "Posts" ON "favorite_posts"."post_id" = "Posts"."id"
-                                 LEFT JOIN "Users" ON "favorite_posts"."user_id" = "Users"."id"
-                                 left JOIN "comments" ON "favorite_posts"."post_id" = "comments"."post_id"
-                        WHERE "favorite_posts"."user_id" = ${user_id} LIMIT ${limit} OFFSET ${offset}`)
+        const [value, metadata] = await FavPost.sequelize.query(`WITH "commentsUsers" AS (
+            SELECT json_agg(json_build_object('commentsId', c.id, 'userId', c.user_id, 'userName', u."firstName", 'text',
+                                              c.text, 'time', c."createdAt")) ,
+                   c.post_id
+            FROM "comments" c
+                     LEFT JOIN "Users" U ON U.id = c.user_id
+            GROUP BY post_id
+        )
+        SELECT pt.id as "postId",
+               p.user_id,
+               pt.title,
+               pt.subtitle,
+               pt.description,
+               pt.image_url,
+               pt."createdAt",
+               U."firstName",
+               cU.*
+        FROM "favorite_posts" P
+                 LEFT JOIN "Users" U on U.id = P.user_id
+                 left join "Posts" pt on pt."id" = p.post_id 
+                 LEFT JOIN "commentsUsers" cU on pt.id = cU.post_id
+        where p.user_id = ${user_id}
+        order by "createdAt" desc
+        limit ${limit} offset ${offset} `)
         return value
     };
 
